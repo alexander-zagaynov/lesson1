1b31f93b14c0768b1777bc9442e2b0d6ef458696
alexander zagaynov <alexander.zagaynov@gmail.com>
2023-05-21 16:55:23 +0300

    1 семинар_задачи

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+// dotnet new console  
+
+/* 1 задача 
+Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 5; b = 25 -> да a = 2 b = 10 -> нет a = 9; b = -3 -> нет a = -3 b = 9 -> да
+
+int input = int.Parse(Console.ReadLine());
+int result = input * input;
+Console.WriteLine(result);
+*/
+
+/* 2 задача
+Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда 5 -> Пятница
+*/
+
+
+
